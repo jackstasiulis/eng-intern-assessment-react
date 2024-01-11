1b8ed2f083624dc0b71416be6d530badcba45697
jackstasiulis <jackstasiulis@gmail.com>
2024-01-11 15:14:51 -0500

    initial commit - jack stasiulis

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function App() {
     return(
         <div>
-            <h1>jack stasiulis</h1>
+            <h1>jack stasiulis - stopwatch</h1>
 
         </div>
     )
